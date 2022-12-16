79c09c529e51b69e7590955ff0ecdb2008d752d1
Colton Donnelly <colton@donn.io>
2022-12-16 19:09:58 +0000

    start with http service

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 #[macro_use]
 extern crate actix_web;
 
+use actix_web::web;
+use actix_web::App;
+use actix_web::HttpResponse;
+use actix_web::HttpServer;
+
 #[actix_web::main]
-fn main() {
-	println!("Hello, world!");
+async fn main() -> std::io::Result<()> {
+	HttpServer::new(|| App::new().service(eval))
+		.bind(("127.0.0.1", 80))?
+		.run()
+		.await
+}
+
+#[post("/eval/{session}/{lang}")]
+async fn eval(path: web::Path<(u64, String)>) -> std::io::Result<HttpResponse> {
+	todo!()
 }
